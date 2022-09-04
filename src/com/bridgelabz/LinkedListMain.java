6c3199a04cb9f94e23e9cebe1deffd206032cfed
priya <priyapandey8884.blr@gmail.com>
2022-09-05 00:56:45 +0530

    to Create a simple linked list and insert a value

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		
+		LinkedList list = new LinkedList();
+		
+		//to add the nodes to the list
+		list.toInsertData(56);
+		list.toInsertData(30);
+		list.toInsertData(70);
+		
+		list.toDisplay();
+	}
+}
---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,29 @@
+package com.bridgelabz;
+
+public class Node<T> {
+	
+	 T data ;
+	 Node<T> next;
+	
+	
+	/**
+	 * constructor
+	 * by default initially data is 
+	 * @param data :data is stored is stored as 0 and next as null
+	 * @param next :initially its stored as null
+	 */
+	public Node(T data) {
+		this.data = data;
+		
+		
+	}
+
+   
+	
+	
+	
+	
+	
+	
+
+}
